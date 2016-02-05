3e4680d54f7c9da641922b54349653febff55d1f
Theothorn <theothorn@liebt-dich.info>
2016-02-05 14:26:17 +0000

    adding files

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>Architektur + Immobilien</title>
+	<link href='http://fonts.googleapis.com/css?family=Bree+Serif|Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
+    <link href="_/css/bootstrap.css" rel="stylesheet">
+	<link href="_/css/bgu_ai.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="home">
+
+          <?php include "_/components/php/header.php"; ?>
+
+    <section class="no-padding" id="portfolio">
+        <div class="container">
+            <div class="row no-gutters">
+                <div class="col-lg-4 col-sm-4">
+                    <a href="Wohnen_im_ehemaligen_Amtssitz_2014.php" class="portfolio-box">
+                        <img src="images/bgu-ai/ai1.jpg" class="img-responsive" alt="Wohnen im ehemaligen Amtssitz 2014">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-name">
+                                    Referenzen
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-4">
+                    <a href="Kulturdenkmal.php" class="portfolio-box">
+                        <img src="images/bgu-ai/ai2.jpg" class="img-responsive" alt="Wohnen am Lindenbach 2011 – 2013">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-name">
+                                    Innenarchitektur
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-4">
+                    <a href="Wohneinheiten_Lindental.php" class="portfolio-box">
+                        <img src="images/bgu-ai/ai3.jpg" class="img-responsive" alt="Wohnen mit Familie und Freunde 2011 – 2013">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-name">
+                                    Verkauf
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-4">
+                    <a href="Wohneinheiten_Lindental.php" class="portfolio-box">
+                        <img src="images/bgu-ai/ai4.jpg" class="img-responsive" alt="Wohnen mitten in der Stadt 2010">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-name">
+                                    Architektur
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-4">
+                    <a href="#" class="portfolio-box">
+                        <img src="images/bgu-ai/ai5.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-name">
+                                    Vermietung
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-4">
+                    <a href="mailto:birgit@goetz-ullrich.de" class="portfolio-box">
+                        <img src="images/bgu-ai/ai6.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-name">
+                                    birgit@goetz-ullrich.de
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                
+            </div><!-- row -->
+        </div>
+
+	</section>
+
+
+    <script src="_/js/bootstrap.min.js"></script>    
+	<script src="_/js/main.min.js"></script>
+	
+
+</body>
+
+</html>
