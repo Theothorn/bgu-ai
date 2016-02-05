3cef3755495ae7ec1fab3bc66a337b0b249a53c6
Theothorn <theothorn@liebt-dich.info>
2016-02-05 13:14:51 +0000

    1. commit

---
+++ bgu-ai/_/components/php/ueber_mich.php
@@ -0,0 +1,43 @@
+<div class="container about">
+  <div class="row">
+	<div class="col-xs-12 col-lg-12">
+<h4> Dipl.-Ing. Birgit Götz-Ullrich<br />Architektin und Innenarchitektin</h4> 
+<p>Neue Weinsteige 37, 70180 Stuttgart<br />
+Mobil +49 172 7101988<br />
+birgit@goetz-ullrich.de</p><br />
+
+<h4>Kompetenz</h4>
+<p>ganzheitliche Bearbeitung von Projekten<br />
+vom Entwurf bis zur baulichen<br />
+Realisierung, vom Städtebau bis zur<br />
+Innenarchitektur.</p><br />
+
+<h4>Schwerpunkte</h4>
+
+<ul class="dashed">
+<li> Private Wohnhäuser</li>
+<li> gewerblicher Wohnungsbau</li>
+<li> Gewerbebauten</li>
+<li> Kultur und Denkmalpflege</li>
+<li> Raumgestaltung</li>
+<li> Ausstellungen</li>
+</ul><br />
+
+
+
+<h4>Prämisse</h4>
+<p>Architektur ist Lebensgestaltung und jedes<br />
+Bauwerk ist einzigartig. Jede Bauaufgabe<br />
+braucht Mut zum Experiment und<br />
+Begeisterung. Die Kreativität und Offenheit<br />
+stehen in engem Dialog mit technische<br />
+Fachwissen und Teamgeist.</p>
+	
+	</div>
+</div><!--/row-->
+        <div class="rows row-centered">
+        <div class="col-sm-4"></div>
+		<div class="col-sm-4"><br><br><br><a href="#about"><i class="glyphicon glyphicon-circle-arrow-up white"></i></a></div>
+		<div class="col-sm-4"></div>
+    </div>
+  </div><!--/container-->
