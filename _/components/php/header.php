e797ed2a5dff2959b371fb61f52e7996a5ce5d39
Theothorn <theothorn@liebt-dich.info>
2016-02-06 01:52:33 +0000

    Remove duplicated files

--- _/components/php/header.php
+++ _/components/php/header.php
@@ -1,11 +1,11 @@
 <header class="clearfix">
 
-<div class="container-fluid">
+<div class="container">
   <div class="row bguai">
     
       <div class="col-xs-12 col-sm-3 col-md-4 col-lg-4">
         <div class="head-address">
-		Architektur + Immobilien <br /><br />
+		Architektur - Immobilien <br /><br />
 		Dipl.-Ing. Birgit Götz-Ullrich <br />
 		Architektin<br /> 
 		und Innenarchitektin     <br />
@@ -13,42 +13,34 @@
 		70180 Stuttgart<br />
 		Mobil +49 172 7101988<br />
 		birgit@goetz-ullrich.de
-		
 		</div>
       </div>
     <div class="col-xs-12 col-sm-3 col-md-4 col-lg-4">
       <div class="head-promo">
 		Ankauf und Verkauf von Immobilien<br />
-		Objektbewertung<br />
+		Vermietung<br />
 		Individuelle Beratung<br />
 		Bewertung von Immobilien<br />
 		Erstellung von Verkaufsunterlagen<br />
 		Vertragsabwicklung<br />
 		Objektsuche<br />
 		Baubetreuung<br />
-		Marketing<br />
-		Diskretion<br />
-				
-				
-				
-				</div>
+		Marketing
+		</div>
     </div>
     <div class="col-xs-12 col-sm-3 col-md-4 col-lg-4">
       <div class="foot-links pull-right">      
-		<a class="navbar-brandd" href="#"><img src="images/misc/bgu-ain.png" alt="Architektur + Immobilien"></a>
-            </div>
-	  
+			<section id="branding">
+		<a href="index.php"><img src="images/misc/bgu-ai-150.png" alt="Architektur + Immobilien"></a>
+	</section><!-- branding -->
+         </div>
 	  </div>
     </div>
- 
-    
-  	</div><!--/row-->
-  
   </div><!--/container-->
 
 <!-- Navigation -->
     <nav class="navbar navbar-inverse " role="navigation">
-        <div class="container-fluid">
+        <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -61,13 +53,13 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    			<li><a href="index.php">Home</a></li>
-			<li><a href="Referenzen.php">Referenzen</a></li>
-			<li><a href="Innenarchitektur.php">Innenarchitektur</a></li>			
-			<li><a href="Architektur.php">Architektur</a></li>
-			<li><a href="Verkauf.php">Verkauf</a></li>			
-			<li><a href="Vermietung.php">Vermietung</a></li>
-			<li><a href="Kontakt.php">Kontakt</a></li>
+                    			<li><a href="index.php">Home</a></li>	
+			<li><a href="architektur.php">Architektur</a></li>
+			<li><a href="verkauf.php">Verkauf</a></li>			
+			<li><a href="vermietung.php">Vermietung</a></li>
+			<li><a href="denkmalpflege.php">Denkmalpflege</a></li>
+			<li><a href="referenzen.php">Referenzen</a></li>		
+			<li><a href="kontaktformular.php">Kontakt</a></li>
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-   <title>Architektur + Immobilien</title>
+   <title>Architektur - Immobilien</title>
 	<link href='http://fonts.googleapis.com/css?family=Bree+Serif|Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
     <link href="_/css/bootstrap.css" rel="stylesheet">
 	<link href="_/css/bgu_ai.css" rel="stylesheet">
@@ -27,31 +27,19 @@
             <div class="row no-gutters">
                 <div class="col-lg-4 col-sm-4">
                     <a href="Wohnen_im_ehemaligen_Amtssitz_2014.php" class="portfolio-box">
-                        <img src="images/bgu-ai/ai1.jpg" class="img-responsive" alt="Wohnen im ehemaligen Amtssitz 2014">
+                        <img src="images/bgu-ai/ai1.jpg" class="img-thumbnail" alt="Wohnen im ehemaligen Amtssitz 2014">
                         <div class="portfolio-box-caption">
                             <div class="portfolio-box-caption-content">
                                 <div class="project-name">
-                                    Referenzen
+                                    Architektur
                                 </div>
                             </div>
                         </div>
                     </a>
                 </div>
                 <div class="col-lg-4 col-sm-4">
-                    <a href="Kulturdenkmal.php" class="portfolio-box">
-                        <img src="images/bgu-ai/ai2.jpg" class="img-responsive" alt="Wohnen am Lindenbach 2011 – 2013">
-                        <div class="portfolio-box-caption">
-                            <div class="portfolio-box-caption-content">
-                                <div class="project-name">
-                                    Innenarchitektur
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4 col-sm-4">
-                    <a href="Wohneinheiten_Lindental.php" class="portfolio-box">
-                        <img src="images/bgu-ai/ai3.jpg" class="img-responsive" alt="Wohnen mit Familie und Freunde 2011 – 2013">
+                    <a href="Kulturdenkmal.php" class="portfolio-box ">
+                        <img src="images/bgu-ai/ai2.jpg" class="img-thumbnail" alt="Wohnen am Lindenbach 2011 – 2013">
                         <div class="portfolio-box-caption">
                             <div class="portfolio-box-caption-content">
                                 <div class="project-name">
@@ -63,19 +51,7 @@
                 </div>
                 <div class="col-lg-4 col-sm-4">
                     <a href="Wohneinheiten_Lindental.php" class="portfolio-box">
-                        <img src="images/bgu-ai/ai4.jpg" class="img-responsive" alt="Wohnen mitten in der Stadt 2010">
-                        <div class="portfolio-box-caption">
-                            <div class="portfolio-box-caption-content">
-                                <div class="project-name">
-                                    Architektur
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4 col-sm-4">
-                    <a href="#" class="portfolio-box">
-                        <img src="images/bgu-ai/ai5.jpg" class="img-responsive" alt="">
+                        <img src="images/bgu-ai/ai3.jpg" class="img-thumbnail" alt="Wohnen mit Familie und Freunde 2011 – 2013">
                         <div class="portfolio-box-caption">
                             <div class="portfolio-box-caption-content">
                                 <div class="project-name">
@@ -86,12 +62,36 @@
                     </a>
                 </div>
                 <div class="col-lg-4 col-sm-4">
-                    <a href="mailto:birgit@goetz-ullrich.de" class="portfolio-box">
-                        <img src="images/bgu-ai/ai6.jpg" class="img-responsive" alt="">
+                    <a href="Wohneinheiten_Lindental.php" class="portfolio-box">
+                        <img src="images/bgu-ai/ai4.jpg" class="img-thumbnail" alt="Wohnen mitten in der Stadt 2010">
                         <div class="portfolio-box-caption">
                             <div class="portfolio-box-caption-content">
                                 <div class="project-name">
-                                    birgit@goetz-ullrich.de
+                                    Denkmalpflege
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-4">
+                    <a href="#" class="portfolio-box">
+                        <img src="images/bgu-ai/ai5.jpg" class="img-thumbnail" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-name">
+                                    Referenzen
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-4">
+                    <a href="kontaktformular.php" class="portfolio-box">
+                        <img src="images/bgu-ai/ai6.jpg" class="img-thumbnail" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-name">
+                                    Kontaktformular
                                 </div>
                             </div>
                         </div>
@@ -103,6 +103,7 @@
 
 	</section>
 
+          <?php include "_/components/php/footer.php"; ?>
 
     <script src="_/js/bootstrap.min.js"></script>    
 	<script src="_/js/main.min.js"></script>
